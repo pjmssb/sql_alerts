b1389592dbd5d2ed1ddbde3b7ab4b449efce069d
Pablo J. Mogollón Somavilla <pjmssb@hotmail.com>
2023-04-23 20:30:40 +0100

    Update bloquear.sql

--- bloquear.sql
+++ bloquear.sql
@@ -1,2 +1,2 @@
-INSERT INTO public.ip_bloqueadas (id, ip_address_ini, ip_address_fin, reason, is_active, created_at, updated_at)
-VALUES (DEFAULT, '123.123.123.001', '123.123.123.255', 'no_reason', true, NOW(), NOW());
+INSERT INTO public.ip_bloqueadas (ip_address_ini, ip_address_fin, reason, is_active, created_at, updated_at)
+VALUES ('123.123.123.001', '123.123.123.255', 'no_reason', true, NOW(), NOW());
