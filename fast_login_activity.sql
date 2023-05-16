3ccc5b80ddf46be8c1c8ec7cbf3b6883a6b7d75f
Pablo J. Mogollón Somavilla <pjmssb@hotmail.com>
2023-05-16 17:18:26 +0100

    Update fast_login_activity.sql

--- fast_login_activity.sql
+++ fast_login_activity.sql
@@ -1,35 +1,20 @@
-/*
-Alta Velocidad
-*/
-
-WITH time_slices AS (
-    SELECT generate_series(
-        date_trunc('hour', (SELECT max(fecha_hora) FROM binnacle) - '360 minutes'::interval),
-        (SELECT max(fecha_hora) FROM binnacle),
-        '10 minutes'::interval
-    ) AS slice_start,
-    generate_series(
-        date_trunc('hour', (SELECT max(fecha_hora) FROM binnacle) - '360 minutes'::interval) + '10 minutes'::interval,
-        (SELECT max(fecha_hora) FROM binnacle),
-        '10 minutes'::interval
-    ) AS slice_end
-)
 SELECT 
-    current_timestamp AS detection_time,
-    id_votacion,
-    ip_publica,
-    min(fecha_hora) AS ini,
-    max(fecha_hora) AS fin,
-    count(*) AS number_records
-FROM binnacle, time_slices
-WHERE fecha_hora BETWEEN time_slices.slice_start AND time_slices.slice_end
-AND id_votacion = binnacle.id_votacion
-AND ip_publica = binnacle.ip_publica
-GROUP BY id_votacion, ip_publica, time_slices.slice_start
-HAVING count(*) >= 10; --100;
+    b.id_votacion,
+    b.ip_publica,
+    'to_fast' AS event,
+    COUNT(*) 
+FROM 
+    binnacle b
+JOIN 
+    voting v ON b.id_votacion = v.id
+WHERE 
+    b.evento = 'ingreso_papeleta' 
+    AND b.fecha_hora >= (CURRENT_TIMESTAMP - INTERVAL '5 minutes') 
+    AND v.state_id >= 4 
+    AND v.date_end >= (CURRENT_DATE - INTERVAL '1 day')
+GROUP BY 
+    b.id_votacion, 
+    b.ip_publica
+HAVING 
+    COUNT(*) > 10;
 
-
-/* 
-Muchos login de la misma IP
-
-*/
