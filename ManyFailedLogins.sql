d9abdb31af284fb66abb23096b8153d34deafae5
Pablo J. Mogollón Somavilla <pjmssb@hotmail.com>
2023-05-16 22:24:32 +0100

    Update ManyFailedLogins.sql

--- ManyFailedLogins.sql
+++ ManyFailedLogins.sql
@@ -1,35 +1,42 @@
+As a seasoned developer with deep knowledge of PostgreSQL, you will code a set of independent SQL scripts, reading from the following table
+binnacle (
+	id int4 NOT NULL,
+	sistema varchar(255) NULL DEFAULT NULL::character varying,
+	usuario int4 NULL,
+	id_votacion int4 NULL,
+	modulo varchar(255) NULL DEFAULT NULL::character varying,
+	evento varchar(255) NULL DEFAULT NULL::character varying,
+	ip_publica varchar(255) NULL DEFAULT NULL::character varying,
+	"json" json NULL,
+	fecha_hora timestamp(0) NULL DEFAULT NULL::timestamp without time zone,
+	CONSTRAINT binnacle_pkey PRIMARY KEY (id)
+);
+the id is a Postgresql sequence named binnacle_id_seq.
 
-DO $$ 
-DECLARE
-    detection_time TIMESTAMP;
-    id_votacion INT;
-    ip_publica VARCHAR(255);
-    evento VARCHAR(255);
-    ini TIMESTAMP;
-    fin TIMESTAMP;
-    number_records INT;
-BEGIN
-    detection_time := NOW();
+and the table 
+voting (
+	id int4 NOT NULL,
+	user_id int4 NULL,
+	state_id int4 NOT NULL,
+	order_type_point_id int4 NULL,
+	"name" varchar(255) NOT NULL,
+	description varchar(255) NULL DEFAULT NULL::character varying,
+	date_start timestamp(0) NOT NULL,
+	date_end timestamp(0) NOT NULL,
+	date_end_census timestamp(0) NOT NULL,
+	logo varchar(255) NULL,
+	public_key text NULL,
+	deleted_at timestamp(0) NULL DEFAULT NULL::timestamp without time zone,
+	is_private bool NULL DEFAULT false,
+	custom_url varchar(20) NULL DEFAULT NULL::character varying,
+	is_large_ballot bool NULL DEFAULT false,
+	access_type json NOT NULL DEFAULT '[]'::json,
+	validation_type int4 NOT NULL DEFAULT 1,
+	CONSTRAINT voting_pkey PRIMARY KEY (id)
+);
 
-    FOR id_votacion, ip_publica, evento, ini, fin, number_records IN
-        SELECT 
-            b.id_votacion,
-            b.ip_publica,
-            b.evento,
-            min(b.fecha_hora) as ini,
-            max(b.fecha_hora) as fin,
-            count(*) as number_records
-        FROM 
-            binnacle b
-        WHERE 
-            b.fecha_hora > '2023-04-03 17:00:00'::timestamp
-            AND b.evento LIKE 'Cliente_Fallo_Login_%'
-        GROUP BY 
-            b.id_votacion, b.ip_publica, b.evento, date_trunc('hour', b.fecha_hora) + INTERVAL '10 min' * floor(extract('minute' FROM b.fecha_hora) / 10)
-        HAVING 
-            count(*) >= 20
-    LOOP
-        -- Perform any required actions with the results here
-        RAISE NOTICE 'id_votacion: %, ip_publica: %, evento: %, ini: %, fin: %, number_records: %', id_votacion, ip_publica, evento, ini, fin, number_records;
-    END LOOP;
-END $$;
+A strong relationship exists between binnacle and voting tables exists defined by voting.id = binnacle.id_votacion.
+
+get time slices of 5 minutes since yesterday at noon. For each time slice, get from binnacle the ip_public, "too_many_failed_logins" as event, count(*)  grouped by id_votacion, ip_publica when binnacle.evento like 'Cliente_Fallo_Login_%' and binnacle.fecha_hora  >=  5 minutes before now and voting.state_id >= 4 and voting.date_end >= yesterday. Extract only the results with count(*) > 5
+
+Ask me for any additional information you need to provide the best code.
