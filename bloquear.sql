e5fdfc15db168817157fe2af05b7d18a6d95a43d
Pablo J. Mogollón Somavilla <pjmssb@hotmail.com>
2023-05-18 10:53:37 +0100

    Update bloquear.sql
    Corrected to address serial id values

--- bloquear.sql
+++ bloquear.sql
@@ -1,2 +1,23 @@
-INSERT INTO public.ip_bloqueadas (ip_address_ini, ip_address_fin, reason, is_active, created_at, updated_at)
-VALUES ('123.123.123.001', '123.123.123.255', 'no_reason', true, NOW(), NOW());
+select setval('ip_bloqueadas_id_seq', (select max(id) from ip_bloqueadas));
+
+INSERT INTO public.ip_bloqueadas (
+  id,   
+  ip_address_ini, 
+  ip_address_fin, 
+  reason, 
+  is_active, 
+  created_at, 
+  updated_at, 
+  voting_id, 
+  rejection_count
+) VALUES (
+  nextval('ip_bloqueadas_id_seq'),
+  '172.121.255.0', 
+  '172.121.255.255', 
+  'SM', 
+  true, 
+  now(), 
+  now(), 
+  null, 
+  0
+);
